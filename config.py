5efebfd2d84d43304db6a8a166befad52eff137f
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-07-25 02:01:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ DONATION_LINK = os.environ.get("https://t.me/i_14344")
 # Wall api key for wallpapers # From:- https://wall.alphacoders.com/api.php
 WALL_API = os.environ.get("WALL_API", None) 
 
-To remove background of images # From:- https://www.remove.bg/
+#To remove background of images # From:- https://www.remove.bg/
 REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", "")
 
 ## More info written at right side from this line.
